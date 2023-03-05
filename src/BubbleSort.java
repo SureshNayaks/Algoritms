771f1facb16dc058664b27db329fa6a38738aee1
suresh kumar jatoth <jatoth@kaleidofin.com>
2023-03-05 18:29:25 +0530

    Push to modified leetcode

---
+++ src/BubbleSort.java
@@ -0,0 +1,21 @@
+public class BubbleSort {
+    public static void main(String[] args) {
+        int[] arr = {10, 9, 8, 7, 6, 5, 4, 3, 2, 1};
+        bubbleSort(arr);
+        for (int j : arr) {
+            System.out.println(j);
+        }
+    }
+
+    private static void bubbleSort(int[] arr) {
+        for (int i = 0; i < arr.length - 1; i++) {
+            for (int j = 0; j < arr.length - i - 1; j++) {
+                if (arr[j] > arr[j + 1]) {
+                    int temp = arr[j];
+                    arr[j] = arr[j + 1];
+                    arr[j + 1] = temp;
+                }
+            }
+        }
+    }
+}
