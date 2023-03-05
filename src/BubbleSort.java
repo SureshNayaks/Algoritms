f9cabc7dab3384b3d1d28e790243fdccb5910f87
suresh kumar jatoth <jatoth@kaleidofin.com>
2023-03-05 14:21:08 +0530

    Push to leetcode

---
+++ src/BubbleSort.java
@@ -0,0 +1,13 @@
+public class BubbleSort {
+    public static void main(String[] args) {
+        int[] arr = {10, 9, 8, 7, 6, 5, 4, 3, 2, 1};
+        bubbleSort(arr);
+        for (int j : arr) {
+            System.out.println(j);
+        }
+    }
+
+    private static void bubbleSort(int[] arr) {
+
+    }
+}
---
+++ src/InsertionSort.java
@@ -0,0 +1,22 @@
+import java.util.Arrays;
+
+
+public class InsertionSort {
+    public static void main(String[] args) {
+        int[] arr = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+        insertionSort(arr);
+        System.out.println(Arrays.toString(arr));
+    }
+
+    private static void insertionSort(int[] arr) {
+        for (int i = 1; i < arr.length; i++) {
+            int key = arr[i];
+            int j = i - 1;
+            while (j >= 0 && arr[j] > key) {
+                arr[j + 1] = arr[j];
+                j--;
+            }
+            arr[j + 1] = key;
+        }
+    }
+}
